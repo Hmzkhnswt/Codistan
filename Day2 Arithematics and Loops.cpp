4b5aeddc40353f11b4b3c0a476f779a0c4310160
Hamza Ali <95092387+Hmzkhnswt@users.noreply.github.com>
2023-12-25 22:50:19 +0500

    Add files via upload

---
+++ Day2 Arithematics and Loops.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int F;
+	cout<<"Enter Temperature in FarenHiet: ";
+	cin>>F;
+	cout<<"The Temperature in Celcius is: "<<(F-32)*5/9;
+	cout<<"The Temperature in Kelnin is: "<<(F-32)*5/9+273.15;
+	return 0;
+}
+
+
+
+//#include<iostream>
+//using namespace std;
+//int main(){
+//	int i ;
+//	for(i=1; i<11; i++){
+//		cout<<"Allah O Akbar!"<<endl;
+//	}
+//	return 0;
+//}
---
+++ Day2 Arithematics and Loops.py
@@ -0,0 +1,10 @@
+F = float(input("Enter Temperature in Fahrenheit: "))
+
+# Conversion formulas
+C = (F - 32) * 5 / 9  # Fahrenheit to Celsius
+K = (F - 32) * 5 / 9 + 273.15  # Fahrenheit to Kelvin
+
+print("The Temperature in Celsius is:", C)
+print("The Temperature in Kelvin is:", K)
+
+

33157941b9aeb29007c7bde460b47d52fdbfec86
Hamza Ali <95092387+Hmzkhnswt@users.noreply.github.com>
2023-12-25 22:42:46 +0500

    Add files via upload

---
+++ Day1_Arithematics.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+	cout<<"The mod of two numbers is: "<<(9%4)+(3%1);
+	return 0;
+}
---
+++ Day1_Arithematics.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter a number1: "))
+num2 = int(input("Enter another number2: "))
+
+print("The Result of two numbers is: ", (num1 % num2)*(num1//num2))
+
+
+# if num = 8 and num2 = 3 
+# ( 8 % 3 = 2 ) * ( 8 // 3 = 2 )
+# 2 * 2 = 4
